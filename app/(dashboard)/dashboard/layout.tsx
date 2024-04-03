7600c9da53b24605a2535321e19d8bcdfda6c47a
nahidtanvir <71749007+nahidtanvir@users.noreply.github.com>
2024-04-03 20:10:44 +0600

    collapsible sidebar rendering without useCheckActive hook and with runtime errors

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,22 +1,28 @@
+"use client";
+
 import Header from "@/components/layout/header";
 import Sidebar from "@/components/layout/sidebar";
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
+import SidebarTwo from "@/components/layout/sidebar-two";
+import useIsCollapsed from '@/hooks/use-is-collapsed'
 
-export const metadata: Metadata = {
-  title: "Next Shadcn Dashboard Starter",
-  description: "Basic dashboard with Next.js and Shadcn",
-};
+// export const metadata: Metadata = {
+//   title: "Next Shadcn Dashboard Starter",
+//   description: "Basic dashboard with Next.js and Shadcn",
+// };
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isCollapsed, setIsCollapsed] = useIsCollapsed()
   return (
     <>
       <Header />
       <div className="flex h-screen overflow-hidden">
-        <Sidebar />
+        {/*<Sidebar />*/}
+        <SidebarTwo isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
         <main className="w-full pt-16">{children}</main>
       </div>
     </>
